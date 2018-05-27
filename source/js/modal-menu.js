35f43b3d683448095e7efe8a4028b0328b57b617
RealRather <36735076+RealRather@users.noreply.github.com>
2018-05-27 21:37:29 +0300

    Изменяет поведение modal-menu.js

--- source/js/modal-menu.js
+++ source/js/modal-menu.js
@@ -2,9 +2,9 @@ var navMenu = document.querySelector(".main-nav");
 var mainToogle = document.querySelector(".main-nav__toggle");
 navMenu.classList.remove("main-nav--withoutjs");
 mainToogle.addEventListener('click', function() {
-  if (navMenu.classList.contains("main-nav--open")) {
-    navMenu.classList.remove("main-nav--open");
-    navMenu.classList.add("main-nav--close");
-  } else {
+  if (navMenu.classList.contains("main-nav--close")) {
+    navMenu.classList.remove("main-nav--close");
     navMenu.classList.add("main-nav--open");
-    navMenu.classList.remove("main-nav--close");  }});
+  } else {
+    navMenu.classList.add("main-nav--close");
+    navMenu.classList.remove("main-nav--open");  }});
